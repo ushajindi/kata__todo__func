12d6fcf538cf8737b3697eb4e81eebdd91138df9
ushajindi <ushajindi@gmail.com>
2023-11-09 17:23:56 +0300

    fixed bugs

--- src/Components/TodoFilter/TodoFilter.tsx
+++ src/Components/TodoFilter/TodoFilter.tsx
@@ -1,50 +1,65 @@
-import {filterType, todoElement} from "../../App.tsx";
+import { FilterType, TodoElement } from "../../App.tsx";
 import React from "react";
 
-type propsType={
-    filter:filterType,
-    setFilter:React.Dispatch<React.SetStateAction<filterType>>,
-    todos:todoElement[],
-    setTodos:React.Dispatch<React.SetStateAction<todoElement[]>>
-}
-function TodoFilter({filter,setFilter,todos,setTodos}:propsType) {
-    const onClearCompliteTodos=()=>{
-        setTodos((prevTodos)=>{
-            return prevTodos.filter(el=>{
-                return el.task !== "Completed";
-
-            })
-        })
-    }
-    const selectedFilter=(filter:filterType)=>{
-        setFilter(filter)
-    }
-    return (
-        <footer className="footer">
-            <span className="todo-count">{
-                todos.reduce((acc,item)=>{
-                    if (item.task==="Completed") return ++acc
-                    return acc
-                },0)
-            } items left</span>
-            <ul className="filters">
-                {["All","Active","Completed"].map((el:string)=>{
-                    if(el===filter){
-                        return (<li key={el}>
-                            <button className="selected">{el}</button>
-                        </li>)
-                    }else{
-                        return (<li key={el}>
-                            <button onClick={()=>{
-                                selectedFilter(el as filterType)
-                            }}>{el}</button>
-                        </li>)
-                    }
-                })}
-            </ul>
-            <button onClick={()=>{onClearCompliteTodos()}} className="clear-completed">Clear completed</button>
-        </footer>
-    );
+type PropsType = {
+  filter: FilterType;
+  setFilter: React.Dispatch<React.SetStateAction<FilterType>>;
+  todos: TodoElement[];
+  setTodos: React.Dispatch<React.SetStateAction<TodoElement[]>>;
+};
+function TodoFilter({ filter, setFilter, todos, setTodos }: PropsType) {
+  const onClearCompliteTodos = () => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((el) => {
+        return el.task !== "Completed";
+      });
+    });
+  };
+  const selectedFilter = (fl: FilterType) => {
+    setFilter(fl);
+  };
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        {todos.reduce((acc, item) => {
+          if (item.task === "Completed") return ++acc;
+          return acc;
+        }, 0)}{" "}
+        items left
+      </span>
+      <ul className="filters">
+        {["All", "Active", "Completed"].map((el: string) => {
+          if (el === filter) {
+            return (
+              <li key={el}>
+                <button className="selected">{el}</button>
+              </li>
+            );
+          } else {
+            return (
+              <li key={el}>
+                <button
+                  onClick={() => {
+                    selectedFilter(el as FilterType);
+                  }}
+                >
+                  {el}
+                </button>
+              </li>
+            );
+          }
+        })}
+      </ul>
+      <button
+        onClick={() => {
+          onClearCompliteTodos();
+        }}
+        className="clear-completed"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
 }
 
 export default TodoFilter;
